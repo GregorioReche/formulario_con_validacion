bbbe19cee255b2ce4facc34310c7ccc0449db8fa
Gregorio Reche <greche@DH.digitalhouse.com>
2019-10-10 12:44:22 -0300

    minor fixes

--- funciones.php
+++ funciones.php
@@ -1,61 +1,61 @@
 <?php
 
 function validarRegistracion($datos) {
-  $errores = [];
+$errores = [];
 
-  //Validar nombre. Valida > 4 caracteres.
-  if (strlen($datos["name"]) < 5){
-    $errores["name"] = "El nombre debe contener más de 4 caracteres";
-  }
+//Validar nombre. Valida > 4 caracteres.
+if (strlen($datos["name"]) < 5){
+  $errores["name"] = "El nombre debe contener más de 4 caracteres";
+}
 
-  //Validar gender
-  if (!isset($datos["gender"])){
-    $errores["gender"] = "Debe seleccionar un gender";
-  }
+//Validar gender
+if (!isset($datos["gender"])){
+  $errores["gender"] = "Debe seleccionar un gender";
+}
 
-  //Validar password
-  if ($datos["password"] == ""){
-    $errores["password"] = "Debe completar el password";
-  }
+//Validar password
+if ($datos["password"] == ""){
+  $errores["password"] = "Debe completar el password";
+}
 
-  //Validar confirmacion password completa.
-  if ($datos["cpassword"] == ""){
-    $errores["cpassword"] = "Debe completar la confirmación del password";
-  }
+//Validar confirmacion password completa.
+if ($datos["cpassword"] == ""){
+  $errores["cpassword"] = "Debe completar la confirmación del password";
+}
 
-  //Validar password sea igual a confirmar password.
-  if($datos["password"] != $datos["cpassword"]){
-    $errores["password_conf"] = "Password y confirmacion deben ser iguales";
-  }
+//Validar password sea igual a confirmar password.
+if($datos["password"] != $datos["cpassword"]){
+  $errores["password_conf"] = "Password y confirmacion deben ser iguales";
+}
 
-  //Validar email esté completo.
-  if($datos["email"] == ""){
-    $errores["email"] = "Debe completar el campo email";
-  }
+//Validar email esté completo.
+if($datos["email"] == ""){
+  $errores["email"] = "Debe completar el campo email";
+}
 
-  //Validar email sea un email.
-  if( filter_var($datos["email"], FILTER_VALIDATE_EMAIL) == false ){
-    $errores["email"] = "Debe ingresar un mail válido";
-  }
+//Validar email sea un email.
+if( filter_var($datos["email"], FILTER_VALIDATE_EMAIL) == false ){
+  $errores["email"] = "Debe ingresar un mail válido";
+}
 
-  return $errores;
+return $errores;
 }
 
 
 function validateAge($birthday, $age = 18)
 {
-    // $birthday can be UNIX_TIMESTAMP or just a string-date.
-    if(is_string($birthday)) {
-        $birthday = strtotime($birthday);
-    }
+  // $birthday can be UNIX_TIMESTAMP or just a string-date.
+  if(is_string($birthday)) {
+      $birthday = strtotime($birthday);
+  }
 
-    // check
-    // 31536000 is the number of seconds in a 365 days year.
-    if(time() - $birthday < $age * 31536000)  {
-        return false;
-    }
+  // check
+  // 31536000 is the number of seconds in a 365 days year.
+  if(time() - $birthday < $age * 31536000)  {
+      return false;
+  }
 
-    return true;
+  return true;
 }
 
 ?>
